c626737dfcbd4460640e1b89766df3af54253c30
morentic <mkopp4191@gmail.com>
2024-10-05 10:24:11 +0200

    add comment header and apply design

--- src/Comment.tsx
+++ src/Comment.tsx
@@ -23,19 +23,32 @@ function Comment(comment: Comment) {
   ));
 
   const imgUrl = new URL(comment.user.image.png, import.meta.url).href;
+  const iconUrl = new URL("./images/icon-reply.svg", import.meta.url).href;
 
   return (
-    <div className="comment">
-      <div className="content">
-        <img src={imgUrl} />
-        <Vote count={comment.score}></Vote>
-        <p>{comment.content}</p>
+    <>
+      <div className="comment">
+        <div className="col">
+          <Vote count={comment.score}></Vote>
+        </div>
+        <div className="col">
+          <div className="header">
+            <img src={imgUrl} />
+            <p>{comment.user.username}</p>
+            <p>{comment.createdAt}</p>
+            <button>
+              <object data={iconUrl} type="image/svg+xml"></object>Reply
+            </button>
+          </div>
+          <div className="content">
+            <p>{comment.content}</p>
+          </div>
+        </div>
       </div>
-
       <div className="replies">
         {repliesElements && repliesElements.length > 0 && repliesElements}
       </div>
-    </div>
+    </>
   );
 }
 
